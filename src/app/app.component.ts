d99ddb09bdb576c46eb3fe8e4aaca38d51a222a1
Thadeus <thadeusnyariki94@gmail.com>
2024-01-17 14:28:35 +0300

    :lipstick: header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LandingComponent } from "./pages/website/landing/landing.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, LandingComponent]
 })
 export class AppComponent {
   title = 'thadeus';
