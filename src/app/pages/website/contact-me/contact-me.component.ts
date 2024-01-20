c234389e6236d2dbae63f159234f6a87244acb2b
Thadeus <thadeusnyariki94@gmail.com>
2024-01-20 14:42:24 +0300

    contact-me

--- src/app/pages/website/contact-me/contact-me.component.ts
+++ src/app/pages/website/contact-me/contact-me.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { SliderButtonComponent } from "../../../libs/slider-button/slider-button.component";
 
 @Component({
-  selector: 'app-contact-me',
-  standalone: true,
-  imports: [],
-  templateUrl: './contact-me.component.html',
-  styleUrl: './contact-me.component.scss'
+    selector: 'app-contact-me',
+    standalone: true,
+    templateUrl: './contact-me.component.html',
+    styleUrl: './contact-me.component.scss',
+    imports: [SliderButtonComponent]
 })
 export class ContactMeComponent {
 
