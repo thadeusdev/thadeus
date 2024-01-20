2c100c803fc5576493f915dd196a91a7c2e3d077
Thadeus <thadeusnyariki94@gmail.com>
2024-01-21 00:28:49 +0300

    completed about all the responsives

--- src/app/libs/header/header.component.ts
+++ src/app/libs/header/header.component.ts
@@ -10,5 +10,21 @@ import { RouterOutlet, RouterLink } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  navVariable: boolean = false;
+  nav_icon_variable: boolean = false;
 
+  openNav() {
+    // this.navVariable = true;
+    this.navVariable =! this.navVariable;
+    this.nav_icon_variable =! this.nav_icon_variable;
+  }
+
+  closeNav() {
+    this.navVariable = false;
+    this.nav_icon_variable = false;
+  }
+
+  onLinkClick() {
+    this.closeNav();
+  }
 }
--- src/app/libs/subscribe-newsletter/subscribe-newsletter.component.ts
+++ src/app/libs/subscribe-newsletter/subscribe-newsletter.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CardImageComponent } from "../card-image/card-image.component";
 import { SliderButtonComponent } from "../slider-button/slider-button.component";
 
 @Component({
@@ -7,7 +6,7 @@ import { SliderButtonComponent } from "../slider-button/slider-button.component"
     standalone: true,
     templateUrl: './subscribe-newsletter.component.html',
     styleUrl: './subscribe-newsletter.component.scss',
-    imports: [CardImageComponent, SliderButtonComponent]
+    imports: [SliderButtonComponent]
 })
 export class SubscribeNewsletterComponent {
 
